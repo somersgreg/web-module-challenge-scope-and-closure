e376148d4a57d3c74b63e4b450dabe20ed8b9b63
Greg Somers <somersgreg@yahoo.com>
2020-07-09 00:15:53 -0400

    > Did 4 Tasks

--- index.js
+++ index.js
@@ -28,10 +28,16 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ *    2 declares outside the function. The function has access to it. A Higher-Order function is a function that receives a function as an argument or returns the function as output.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
+ *
+ *    Counter1 has a function inside a function which i think is closure. It has an outer function that is being called by an inner function.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+//  
+ *    1 is better when we dont want our variable to be effected by outside. It is reuseable. Not sure how to describe 2. 
+ * 
 */
 
 // counter1 code
@@ -55,56 +61,81 @@ function counter2() {
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
-
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+console.log(":   :  : :::[TASK 2]::: :  :   :")
+function inning(){  // I DONT GET EXACTLY WHY IM NOT USING A PARAM
+  let points = 0;  // Could I do this so it will add with each repeat but clear if i ask it to?
+    points = Math.floor(Math.random()*3);
+    return points;
 }
-
+console.log(inning())
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
-
 For example, 
-
 finalScore(inning, 9) might return: 
 {
   "Home": 11,
   "Away": 5,
 }
+*/
+console.log(":   :  : :::[TASK 3]::: :  :   :")
 
-*/ 
+// function finalScore(inning,numOfInnings){
+//   let awayTeam = 0;
+//   let homeTeam = 0;
+//     for (let i = 0; i < numOfInnings; i++) {
+//       homeTeam = homeTeam + inning();
+//       awayTeam = awayTeam + inning();}
+//   return {"Home" : homeTeam, 
+//           "Away" : awayTeam}
+//}
+// console.log(finalScore(inning,9));
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inning, inningNum){
+  let home = 0;
+  let away = 0;
+  for(let i = 0; i < inningNum; i++){
+    home  = home  + inning();
+    away  = away  + inning();}
+    return {Home: home,
+            Away: away}
 }
+console.log(finalScore(inning,9));
+
 
 /* Task 4: 
-
 Create a function called `scoreboard` that accepts the following parameters: 
 (1) Callback function `getInningScore`
 (2) Callback function `inning` that you wrote above
 (3) A number of innings
-
 and returns the score at each point in the game, like so:
-
-1st inning: 0 - 2         1st inning: awayTeam - homeTeam
-2nd inning: 1 - 3         2nd inning: awayTeam - homeTeam
-3rd inning: 1 - 3         3rd inning: awayTeam - homeTeam
-4th inning: 2 - 4         4th inning: awayTeam - homeTeam
-5th inning: 4 - 6         5th inning: awayTeam - homeTeam
-6th inning: 4 - 6         6th inning: awayTeam - homeTeam
-7th inning: 4 - 6         7th inning: awayTeam - homeTeam
-8th inning: 5 - 8         8th inning: awayTeam - homeTeam
-9th inning: 6 - 10        9th inning: awayTeam - homeTeam
-
-Final Score: 6 - 10       Final Score: awayTeam - homeTeam */
-
-function scoreboard(getInningScore,inning,num) {
-  /* CODE HERE */
+1st inning: awayTeam - homeTeam
+2nd inning: awayTeam - homeTeam
+3rd inning: awayTeam - homeTeam
+...
+9th inning: awayTeam - homeTeam
+Final Score: awayTeam - homeTeam */
+console.log(":   :  : :::[TASK 4]::: :  :   :")
+function scoreboard(inning,numberInnings) {
+  let score = {
+    'Home': 0,
+    'Away': 0,}
+  for (let i=0; i<numberInnings; i++){
+    score.Home += inning();
+    score.Away += inning();
+function suffix(){  // I can do as a variable?
+  if(i===1){
+    return"st"
+  }else if (i===2){
+    return"nd"
+  }else if (i===3){
+    return"rd"
+  }else {
+    return"th"
+  }
 }
-
-
+    console.log(`${i+1}${suffix()} inning: ${score.Home} - ${score.Away}`);
+  }
+  console.log(`Final Score : ${score.Home} - ${score.Away}`);
+}
+(scoreboard(inning, 9));
